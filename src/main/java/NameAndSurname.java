1a5c3cc166326f2aa48133cc944ee0b4c15deaba
NikolaSiljanovski <siljanovski0@gmail.com>
2024-03-04 10:33:27 +0100

    Add NameAndSurname.java

---
+++ src/main/java/NameAndSurname.java
@@ -0,0 +1,15 @@
+
+public class NameAndSurname {
+
+	public static void main(String[] args) {
+		
+	String name = "Nikola" ;
+	String surname = "Siljanovski" ;
+	
+	
+	System.out.println(name + " " + surname);
+	
+
+	}
+
+}
